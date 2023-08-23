20aa1de62a2414ad58a781a5e2f2967b957a2196
Shajeeb <liveclasswd@gmail.com>
2023-08-23 12:14:16 +0600

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+const app = express();
+
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: any) => {
+  res.send('Hello World!');
+});
+
+export default app;
