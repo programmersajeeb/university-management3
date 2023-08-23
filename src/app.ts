65144857e123362526aefc6b0e20d9d9bd15b3c9
Shajeeb <liveclasswd@gmail.com>
2023-08-23 12:35:05 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
-import express, { Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-const app = express();
+const app: Application = express();
 
+// use cors
 app.use(cors());
 
 // parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
 
 async function bootstrap() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
-    console.log("Database connect successfully");
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    console.log('Database connect successfully');
 
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
-    console.log("Failed to connect the database", error);
+    console.log('Failed to connect the database', error);
   }
 }
 bootstrap();
